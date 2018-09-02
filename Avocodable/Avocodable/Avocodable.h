0f999de0f0cdffee7ededcf8b36a1e80ce7613bd
Nahuel Zapata <inahuelzapata@gmail.com>
2018-09-02 20:10:41 -0300

    Added Swiftlint config. And base project setup

---
+++ Avocodable/Avocodable/Avocodable.h
@@ -0,0 +1,19 @@
+//
+//  Avocodable.h
+//  Avocodable
+//
+//  Created by Nahuel Zapata on 9/2/18.
+//  Copyright © 2018 iNahuelZapata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Avocodable.
+FOUNDATION_EXPORT double AvocodableVersionNumber;
+
+//! Project version string for Avocodable.
+FOUNDATION_EXPORT const unsigned char AvocodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Avocodable/PublicHeader.h>
+
+
---
+++ Avocodable/AvocodableTests/AvocodableTests.swift
@@ -0,0 +1,25 @@
+//
+//  AvocodableTests.swift
+//  AvocodableTests
+//
+//  Created by Nahuel Zapata on 9/2/18.
+//  Copyright © 2018 iNahuelZapata. All rights reserved.
+//
+
+@testable import Avocodable
+import XCTest
+
+class AvocodableTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testPerformanceExample() {
+        self.measure { }
+    }
+}
